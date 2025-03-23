130bc74c5adbe768ce903181a0a7d1e8e3bed937
Gracevaish <vaishnavi.rajput1118@gmail.com>
2025-03-23 00:14:01 +0000

    assignment5

---
+++ cs615_nodejs/2_functions.js
@@ -0,0 +1,13 @@
+function sayHi(){
+    console.log('Hi');
+  }
+  sayHi();
+  var sayBye = function(){
+    console.log('Bye');
+  };
+  sayBye();
+  function callFunction(fun){
+    fun();
+  }
+  callFunction(sayBye);
+      
